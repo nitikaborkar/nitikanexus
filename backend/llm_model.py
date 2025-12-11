cbc6149bd779896ad39161c5f6e80ed09c9a43f4
Nitika Borkar <127076905+nitikaborkar@users.noreply.github.com>
2025-12-12 00:06:52 +0530

    Change default LLM model name to 'gemini-1.5-flash'

--- backend/llm_model.py
+++ backend/llm_model.py
@@ -1,7 +1,7 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 import os
 
-def set_llm_model(model_name="gemini-2.0-flash-exp", temperature: float = 0.7):
+def set_llm_model(model_name="gemini-1.5-flash", temperature: float = 0.7):
     temperature = min(max(temperature, 0.0), 1.0)
     
     return ChatGoogleGenerativeAI(
